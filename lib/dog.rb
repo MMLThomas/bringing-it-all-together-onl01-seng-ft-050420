149460cbec69985c250bb8dceebf33c923c4d7cb
michael thomas <michael.thomas2014@gmail.com>
2020-06-08 19:51:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,6 @@ class Dog
   def self.find_by_name(name)
     row = DB[:conn].execute('SELECT * FROM dogs WHERE name = ?', name).flatten
     dog = Dog.new(id: row[0], name: row[1], breed: row[2])
-    dog
   end
   
   

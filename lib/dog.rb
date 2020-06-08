9390b741b6adf8af9396e1d6912a46cafa7858d2
michael thomas <michael.thomas2014@gmail.com>
2020-06-08 19:47:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,7 @@ class Dog
     if !dog.empty?
       binding.pry
       data = dog[0]
-      dog = Dog.new.new_from_db(data)
+      dog = Dog.new_from_db(data)
     else
       dog = Dog.new.create(row)
     end
